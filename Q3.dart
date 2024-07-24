8a606a806db233ef7751d09e1a0a8923cb1de55d
CodeWithAmmar1 <m.aliammar.naseer@gmail.com>
2024-07-25 03:05:39 +0500

    Q3 done

--- Q3.dart
+++ Q3.dart
@@ -2,6 +2,7 @@ void main() {
   print(
       "Q. 3)  create a list  of name, class, roll no, grade, percentage. And print. ");
   List markSheet = ["Ali Ammar", "2nd Year", 545345, "B", 60.47];
+
   print("NAME:" + markSheet[0]);
   print("CLASS: " + markSheet[1]);
   print("ROLL NO: " + markSheet[2].toString());
