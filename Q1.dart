80974ef66c23cdef90cd6aba0bb6519eaa61a8fd
AliAmmarNaseer <aliammar0342@gmail.com>
2024-07-25 02:56:35 +0500

    assignment 05

---
+++ Q1.dart
@@ -0,0 +1,5 @@
+void main() {
+  print("Q.1) Create a list of names and print all names using list");
+  List<String> names = ["Ali", "Shahzed", "Ammar", "Azam", "Aiman"];
+  print("$names here is list of all names");
+}
