30db9c09c2573c1c8070f9c470e395c5d854e49d
MUHAMMAD ALI AMMAR <m.aliammar.naseer@gmail.com>
2024-08-15 05:14:49 +0500

    Assignment 6 Q1.dart

--- Q1.dart
+++ Q1.dart
@@ -3,7 +3,7 @@ import 'dart:math';
 void main() {
   List<int> number = [1, 4, 6, 7, 3, 9, 8];
   int smallest = number.reduce(min);
-  int largest = number.reduce(max);
+  int largest = number.reduce(max); 
   print("smallest value of list is : $smallest");
   print("largest value of list is : $largest");
 }
